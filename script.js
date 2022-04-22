46dd430b1d80588b5e6d12e3194624392f759eb4
Wagner Gauer <81208157+WagnerGauer@users.noreply.github.com>
2022-04-22 03:43:15 -0300

    show balance, income and expanse

--- script.js
+++ script.js
@@ -1,53 +1,81 @@
 
-// const balance = document.querySelector('#balance')
-// const moneyPlus = document.querySelector('#money-plus')
-// const moneyMinus = document.querySelector('#money-minus')
-// const list = document.querySelector('#list')
-// const form = document.querySelector('#form')
-// const text = document.querySelector('#text')
-// const amount = document.querySelector('#amount')
+const balance = document.querySelector('#balance')
+const moneyPlus = document.querySelector('#money-plus')
+const moneyMinus = document.querySelector('#money-minus')
+const list = document.querySelector('#list')
+const form = document.querySelector('#form')
+const text = document.querySelector('#text')
+const amount = document.querySelector('#amount')
 
-// const dummyTransactions = [ { id: 1, text: 'Flower', amount: -20},
-//     { id: 1, text: 'Flower', amount: -20},
-//      { id: 2, text: 'Salary', amount: 300},
-//     { id: 3, text: 'Book', amount: -20},
-//     { id: 4, text: 'Camera', amount: -20},
-// ]
+const dummyTransactions = [ { id: 1, text: 'Flower', amount: -20},
+    { id: 1, text: 'Flower', amount: -20},
+     { id: 2, text: 'Salary', amount: 300},
+    { id: 3, text: 'Book', amount: -20},
+    { id: 4, text: 'Camera', amount: -20},
+]
 
-// let transactions = dummyTransactions;
+let transactions = dummyTransactions;
 
-// form.addEventListener('submit', (e) => {
-//     console.log(e)
-// })
+form.addEventListener('submit', (e) => {
+    console.log(e)
+})
 
 
 
-// // ADD TRANSACTION TO DOM 
-// function addTransationDOM(transaction) {
-//     console.log(transaction)
-//     // Get sign 
-//     const sign = transaction.amount < 0 ? '-' : '+';
+// ADD TRANSACTION TO DOM 
+function addTransationDOM(transaction) {
+    // console.log(transaction)
+    // Get sign 
+    const sign = transaction.amount < 0 ? '-' : '+';
 
-//     const item = document.createElement('li');
+    const item = document.createElement('li');
 
-//     // Add class based on value 
-//     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+    // Add class based on value 
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
-//     item.innerHTML = `
-//     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
-//     </span> <button class="delete-btn">x</button>   
-//     `;
+    item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
+    </span> <button class="delete-btn">x</button>   
+    `;
 
-//     list.appendChild(item);
+    list.appendChild(item);
 
-// }
+}
 
-// // Init app 
+// Update the balance, income and expense 
+function updateValues() {
+    const amounts = transactions.map(transaction => 
+        transaction.amount
+    )
+        const total = amounts.reduce((acumulator, item) => (acumulator += item), 0)
+        .toFixed(2);
 
-// function init() {
-//     list.innerHTML = ''
+        // toFixed() allows me to choose how many decimal spaces I want 
 
-//     transactions.forEach(addTransationDOM);
-// }
+        const income = amounts
+        .filter(item => item > 0)
+        .reduce((acumulator, item) => (acumulator += item), 0)
+        .toFixed(2);
 
-// init();
+        const expense = (amounts
+        .filter(item => item < 0)
+        .reduce((acumulator, item) => (acumulator += item), 0) * -1)
+        .toFixed(2);
+
+        console.log(expense)
+
+        balance.innerText = `$${total}`;
+        moneyPlus.innerText = `$${income}`;
+        moneyMinus.innerText = `$${expense}`;
+}
+
+// Init app 
+
+function init() {
+    list.innerHTML = ''
+
+    transactions.forEach(addTransationDOM);
+    updateValues();
+}
+
+init();
